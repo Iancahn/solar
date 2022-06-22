e83fe01e6447530216cdaa033b2cc510cc390aa9
Ian Cahn <iancahn@live.com>
2022-06-22 19:11:34 +0200

    Base setup
    Created the base GreenCo setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// Components
+import nav from './component/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function navbar() {
+    return (
+        <h3>Hey again</h3>
+    )
+}
+
+export default navbar;
+
+
